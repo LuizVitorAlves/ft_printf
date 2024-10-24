fa16d9caaa851a937000abf29e6721ab402137b1
Luiz Vitor Alves <116123717+LuizVitorAlves@users.noreply.github.com>
2024-10-23 21:12:48 -0300

    Update ft_printf_pointer.c

--- ft_printf_pointer.c
+++ ft_printf_pointer.c
@@ -1,47 +1,6 @@
 #include "ft_printf.h"
 
 //%p
-
-// Função auxiliar para imprimir uma string de trás para frente
-static void	ft_rprint(char *s)
-{
-	int	i;
-
-	i = ft_strlen(s) - 1;
-	while (i >= 0)
-	{
-		write(1, &s[i], 1);  // Imprime um caractere por vez, de trás para frente
-		i--;
-	}
-}
-
-// Função que converte um número em hexadecimal (minúsculo) e o imprime
-static int	ft_printf_bighex(unsigned long nb)
-{
-	unsigned int	num;
-	char			*base;
-	char			arr[16];  // Armazena o número convertido em hexadecimal
-	int				i;
-
-	base = "0123456789abcdef";  // Base hexadecimal em minúsculas
-	i = 0;
-	if (nb == 0)
-	{
-		ft_putchar_fd('0', 1);  // Imprime 0 se o número for zero
-		return (1);
-	}
-	while (nb)
-	{
-		num = nb % 16;        // Calcula o valor do último dígito em hexadecimal
-		arr[i] = base[num];   // Armazena o dígito correspondente no array
-		nb = nb / 16;         // Divide o número por 16 para processar o próximo dígito
-		i++;
-	}
-	arr[i] = '\0';           // Finaliza a string
-	ft_rprint(arr);          // Imprime a string de trás para frente
-	return (i);              // Retorna o número de dígitos impressos
-}
-
 // Função que imprime um ponteiro no formato hexadecimal
 int	ft_printf_pointer(unsigned long nb)
 {
