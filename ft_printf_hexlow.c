9e9ff93f446e6b3715d35a0b7b29b352f1c0ff5d
Luiz Vitor Alves <116123717+LuizVitorAlves@users.noreply.github.com>
2024-10-23 21:54:00 -0300

    Create ft_printf_hexlow.c

---
+++ ft_printf_hexlow.c
@@ -0,0 +1,12 @@
+static int ft_puthex_lower(unsigned int num)
+{
+    char *base = "0123456789abcdef";  // Para hexadecimal minúsculo
+    int count;
+
+    if (num >= 16)
+    {
+        count = ft_puthex_lower(num / 16) + write(1, &base[num % 16], 1);
+        return (count);
+    }
+    return (write(1, &base[num], 1));  // Escreve o dígito correspondente
+}
