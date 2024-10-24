eb9d6c7e0184fc3930dbee220b9b719a1fd67c4a
Luiz Vitor Alves <116123717+LuizVitorAlves@users.noreply.github.com>
2024-10-23 21:23:35 -0300

    Update ft_printf_pointer.c

--- ft_printf_pointer.c
+++ ft_printf_pointer.c
@@ -2,7 +2,7 @@
 
 //%p
 // Função que imprime um ponteiro no formato hexadecimal
-int ft_putptr(void *ptr)
+int ft_printf_pointer(void *ptr)
 {
     unsigned long long address;
     int count;
@@ -15,6 +15,6 @@ int ft_putptr(void *ptr)
         write(1, "(nil)", 5);
         return (count + 5);  // Retorna a contagem total incluindo "(nil)"
     }
-    count += ft_puthex(address, 0);  // Imprime o valor em hexadecimal minúsculo
+    count += ft_printf_hex(address, 0);  // Imprime o valor em hexadecimal minúsculo
     return (count);
 }
