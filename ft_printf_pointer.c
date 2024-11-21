6d7acdda483c9110f38637254cd914eea2601851
Luiz Vitor Alves <116123717+LuizVitorAlves@users.noreply.github.com>
2024-11-21 13:55:33 -0300

    Update ft_printf_pointer.c

--- ft_printf_pointer.c
+++ ft_printf_pointer.c
@@ -14,19 +14,31 @@
 
 //%p
 //Function that prints a pointer in hexadecimal format
-int	ft_printf_pointer(void *ptr)
-{
-	unsigned long long	address;
-	int					count;
 
-	address = (unsigned long long)ptr;
-	write(1, "0x", 2);
-	count = 2;
-	if (address == 0)
-	{
-		write(1, "(nil)", 5);
-		return (count + 5);
-	}
-	count += ft_printf_hexlow(address);
-	return (count);
+static int	ft_putnbr_hex(unsigned long long num)
+{
+        char    *base;
+        int     count;
+
+        base = "0123456789abcdef";
+        count = 0;
+        if (num >= 16)
+                count += ft_putnbr_hex(num / 16);
+        count += write(1, &base[num % 16], 1);
+        return (count);
+}
+
+int     ft_printf_pointer(void *ptr)
+{
+        unsigned long long      address;
+        int     count;
+
+        if (ptr == NULL)
+        {
+                return (ft_printf_string("(nil)"));
+        }
+        address = (unsigned long long)ptr;
+        count = write(1, "0x", 2);
+        count += ft_putnbr_hex(address);
+        return (count);
 }
